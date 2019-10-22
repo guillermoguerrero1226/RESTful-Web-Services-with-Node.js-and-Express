b57737ff9040da20fd28ae8c55a15652ffcab3fd
William Guerrero <william.guerrero@relativity.com>
2019-10-22 14:12:18 -0500

    Code refactor and implement put and patch verboses

---
+++ routes/bookRouter.js
@@ -0,0 +1,70 @@
+const express = require("express");
+
+function routes(Book){
+    const bookRouter =  express.Router();
+    bookRouter.route("/books")
+    .post((req, res) => {
+        const book = new Book(req.body);
+        book.save();
+        return res.status(201).json(book);
+    })
+
+    .get((req, res) => {
+        const {query} = req;
+        Book.find(query, (err, books) => {
+            if (err) {
+                return res.send(err);
+            }
+            return res.json(books);
+        });
+    });
+
+    bookRouter.use("/books/:bookId", (req, res, next) => {
+        Book.findById(req.params.bookId, (err, book) => {
+            if (err) {
+                return res.send(err);
+            }
+            if(book){
+                req.book = book;
+                return next();
+            }
+            return res.sendStatus(404);
+        });
+    });
+    bookRouter.route("/books/:bookId")
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+        const { book } = req;
+        book.title =  req.body.title;
+        book.author =  req.body.author;
+        book.genre =  req.body.genre;
+        book.read =  req.body.read;
+        req.book.save((err) => {
+            if(err){
+                return res.send(err);
+            }
+            return res.json(book);
+        })
+    })
+    .patch((req, res) => {
+        const { book } = req;
+
+        if(req.body._id){
+            delete req.body._id;
+        }
+        Object.entries(req.body).forEach((prop) => {
+            const key =  prop[0];
+            const value = prop[1];
+            book[key] = value
+        });
+        req.book.save((err) => {
+            if(err){
+                return res.send(err);
+            }
+            return res.json(book);
+        })
+    });
+    return bookRouter;
+}
+
+module.exports = routes;
