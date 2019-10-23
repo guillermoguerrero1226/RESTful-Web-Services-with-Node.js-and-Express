2b09fd547dad67c5e41ce092261268eba2d72f24
Guillermo Guerrero <guillermoguerrero1226@gmail.com>
2019-10-22 22:58:21 -0500

    Add bookController, bookControllerTests and refactor code

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,23 +1,12 @@
 const express = require("express");
+const booksController = require("../controllers/booksController");
 
 function routes(Book){
     const bookRouter =  express.Router();
+    const controller = booksController(Book);
     bookRouter.route("/books")
-    .post((req, res) => {
-        const book = new Book(req.body);
-        book.save();
-        return res.status(201).json(book);
-    })
-
-    .get((req, res) => {
-        const {query} = req;
-        Book.find(query, (err, books) => {
-            if (err) {
-                return res.send(err);
-            }
-            return res.json(books);
-        });
-    });
+    .post(controller.post)
+    .get(controller.get);
 
     bookRouter.use("/books/:bookId", (req, res, next) => {
         Book.findById(req.params.bookId, (err, book) => {
@@ -62,6 +51,14 @@ function routes(Book){
                 return res.send(err);
             }
             return res.json(book);
+        })
+    })
+    .delete((req, res) => {
+        req.book.remove((err) => {
+            if(err){
+                return res.send(err);
+            }
+            return res.sendStatus(204);
         })
     });
     return bookRouter;
